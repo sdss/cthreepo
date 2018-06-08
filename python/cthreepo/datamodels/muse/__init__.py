0396752056c2cdf099313c0d6455209a5884b4ec
Brian Cherinka <havok2063@hotmail.com>
2018-06-08 16:32:01 -0400

    adding some mixin tests; added test muse datamodel; moved objects lists into new core file

---
+++ python/cthreepo/datamodels/muse/__init__.py
@@ -0,0 +1,11 @@
+# !usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under a 3-clause BSD license.
+#
+# @Author: Brian Cherinka
+# @Date:   2018-06-08 15:02:40
+# @Last modified by:   Brian Cherinka
+# @Last Modified time: 2018-06-08 15:11:58
+
+from __future__ import print_function, division, absolute_import
---
+++ python/cthreepo/datamodels/muse/release.py
@@ -0,0 +1,35 @@
+# !usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under a 3-clause BSD license.
+#
+# @Author: Brian Cherinka
+# @Date:   2018-06-08 15:11:48
+# @Last modified by:   Brian Cherinka
+# @Last Modified time: 2018-06-08 15:12:03
+
+from __future__ import print_function, division, absolute_import
+from astropy import units as u
+
+from cthreepo.core.objects import DataCube
+from cthreepo.core.mixins import FitsMixin
+from cthreepo.datamodels.muse.base import MuseDataModel, MuseDataModelList
+
+spaxel_unit = u.Unit('spaxel', represents=u.pixel, doc='A spectral pixel', parse_strict='silent')
+
+
+class MuseDataCube(DataCube, FitsMixin):
+    pass
+
+
+muse_datacubes = [
+    MuseDataCube('flux', extension_name='FLUX', extension_std='STAT',
+                 unit=u.erg / u.s / (u.cm ** 2) / u.Angstrom / spaxel_unit,
+                 scale=1e-20, formats={'string': 'Flux'},
+                 description='3D rectified cube')
+]
+
+DR1 = MuseDataModel('DR1', aliases=['DR1', 'v1_0_0'], datacubes=muse_datacubes)
+
+datamodel = MuseDataModelList([DR1])
+
