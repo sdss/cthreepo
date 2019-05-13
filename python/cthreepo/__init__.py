f2f9d2149763c672752bb6290eb3eb1aa5a35772
Brian Cherinka <havok2063@hotmail.com>
2019-05-13 18:37:36 -0400

    fixing yaml loader

--- python/cthreepo/__init__.py
+++ python/cthreepo/__init__.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 
 import os
 import yaml
+from pkg_resources import parse_version
 
 # Inits the logging system. Only shell logging, and exception and warning catching.
 # File logging can be started by calling log.start_file_logger(name).
@@ -30,15 +31,17 @@ NAME = 'cthreepo'
 
 
 # Loads config
+yaml_version = parse_version(yaml.__version__)
+loader = yaml.FullLoader if yaml_version >= parse_version('5.1') else yaml.SafeLoader
 with open(os.path.dirname(__file__) + '/etc/{0}.yml'.format(NAME)) as ff:
-    config = yaml.load(ff, Loader=yaml.FullLoader)
+    config = yaml.load(ff, Loader=loader)
 
 
 # If there is a custom configuration file, updates the defaults using it.
 custom_config_fn = os.path.expanduser('~/.{0}/{0}.yml'.format(NAME))
 if os.path.exists(custom_config_fn):
     with open(custom_config_fn) as ff:
-        config = merge(yaml.load(ff, Loader=yaml.FullLoader), config)
+        config = merge(yaml.load(ff, Loader=loader), config)
 
 
 __version__ = '0.1.0dev'
